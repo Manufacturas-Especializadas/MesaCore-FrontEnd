ba2ce67fd2f7924f691431adcb6b9b8b4b61da9a
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-04-07 09:42:10 -0600

    Se coloco el usuario, rol y la funcion de cerrar sesion al navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import { AppBar, Box, Menu, IconButton, MenuItem, Toolbar, Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ handleDrawerOpen }) => {
-    const [auth, setAuth] = useState(true);
     const [anchorEl, setAnchorEl] = useState(null);
+    const [userEmail, setUserEmail] = useState("");
+    const [userRole, setUserRole] = useState("");
+    const navigate = useNavigate();
+
+    const handleNavigate = (path) => {
+        navigate(path);
+    }
 
     const handleChange = (e) =>{
         setAnchorEl(e.target.checked);
@@ -18,6 +26,24 @@ const Navbar = ({ handleDrawerOpen }) => {
     const handleClose = () => {
         setAnchorEl(null);
     };
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        handleNavigate("/");
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if(token){
+            try{
+                const decoded = jwtDecode(token);
+                setUserEmail(decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"] || "");
+                setUserRole(decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] || "")
+            }catch(error){
+                console.error(`Token invalido: ${error}`);
+            }
+        }
+    }, []);
     
     return (
         <>
@@ -37,41 +63,44 @@ const Navbar = ({ handleDrawerOpen }) => {
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                             MESA - FX
                         </Typography>
-                        {
-                            auth && (
-                                <div>
-                                    <IconButton
-                                        size="large"
-                                        aria-label="account of current user"
-                                        aria-controls="menu-appbar"
-                                        aria-haspopup="true"
-                                        onClick={ handleMenu }
-                                        color="inherit"
-                                    >
-                                        <AccountCircle/>
-                                    </IconButton>
 
-                                    <Menu
-                                        id="menu-appbar"
-                                        anchorEl={ anchorEl }
-                                        anchorOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right',
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right',
-                                        }}
-                                        open={ Boolean( anchorEl )}
-                                        onClose={ handleClose }
-                                    >
-                                        <MenuItem onClick={ handleClose }>Profile</MenuItem>
-                                        <MenuItem onClick={ handleClose }>My account</MenuItem>
-                                    </Menu>
-                                </div>
-                            )
-                        }
+                            <div>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={ handleMenu }
+                                    color="inherit"
+                                >
+                                    <AccountCircle/>
+                                </IconButton>
+
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={ anchorEl }
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={ Boolean( anchorEl )}
+                                    onClose={ handleClose }
+                                >
+                                    <MenuItem disabled>
+                                        { userEmail }
+                                    </MenuItem>
+                                    <MenuItem disabled>
+                                        <strong style={{ marginLeft: '5px' }}> { userRole } </strong>
+                                    </MenuItem>
+                                    <MenuItem onClick={ handleLogout }> Cerrar sesión </MenuItem>
+                                </Menu>
+                            </div>
+
                     </Toolbar>
                 </AppBar>
             </Box>
