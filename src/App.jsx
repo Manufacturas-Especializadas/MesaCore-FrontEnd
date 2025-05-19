a63cb80fcb701bd471249b3c251fcbccafea546c
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-05-19 11:27:16 -0600

    navbar y sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 
 import MyRoutes from "./Routes/Routes";
+import Navbar2 from "./components/Navbar/Navbar2";
+import Sidebar2 from "./components/Sidebar/Sidebar2";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -18,13 +20,22 @@ function App() {
     setOpen(false);
   };
 
+  // const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+  // const toggleSidebar = () => {
+  //   setIsMobileOpen(!isMobileOpen);
+  // };
+
   return (
     <>
       <BrowserRouter>
         <Navbar handleDrawerOpen={ handleDrawerOpen }/>
         <Sidebar open={ open } handleDrawerClose={ handleDrawerClose }/>
+
+        {/* <Navbar2 toggleSidebar={toggleSidebar} isMobileOpen={isMobileOpen} />
+        <Sidebar2 isMobileOpen={ isMobileOpen } toggleSidebar={ toggleSidebar }/> */}
         
-        <Box sx={{marginTop: 2, marginLeft: 2}}>
+        <Box sx={{marginTop: 10, marginLeft: 10,}}>
           <MyRoutes/>
         </Box>
       </BrowserRouter>
